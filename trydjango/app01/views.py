a1bd882a331063b1f5c4f79c82187c66046110b0
meltice0v0 <jiuxiang9x@gmail.com>
2024-03-02 15:05:59 +0800

    初步实现

--- trydjango/app01/views.py
+++ trydjango/app01/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the app01 index.")
+    return render(request, "showpicture.html")
--- trydjango/trydjango/urls.py
+++ trydjango/trydjango/urls.py
@@ -21,5 +21,5 @@ from app01 import views
 urlpatterns = [
     #path("admin/", admin.site.urls),
     
-    path("index/", views.index),
+    path("", views.index),
 ]
